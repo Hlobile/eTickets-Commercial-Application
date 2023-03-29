28381eebcc5bb6082d84cdacd9a9a525a3d692e2
hlobile <shongwehlobile@gmail.com>
2023-03-29 15:43:50 +0200

    Add project files.

---
+++ eTicketsApplication/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace eTicketsApplication.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
